71ab9d853db467df7ef0fbc7b8321cbdca2872bb
abdulaziz <abdulazizalaa@ymail.com>
2014-12-21 14:25:16 +0200

    front-end fixes
    -website starts to fetch restaurants as the document is ready
    -fixed issue with the restaurants boxes caused by the unequal height
    -did some code-refactoring in app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+//html for the meal items
 var modalContent =  '<div class="row text-center">'+
                       '<div class="col-xs-2"><img src="%imagepath%" class="img-responsive" ></div>'+
                       '<div class="col-xs-3" style="margin-top: 2%"><p3>%name%</p3></div>'+
@@ -6,22 +6,19 @@ var modalContent =  '<div class="row text-center">'+
                       '<div class="col-xs-2" style="margin-top: 2%;"><button type="button" class="btn-confirm"><p2>Add</p2></button></div>'+
                       '<div class="col-md-3 col-sm-3 col-xs-3" style="margin-top: 2%"><button type="button" class="btn-confirm"><p2>Remove</p2></button></div>'+
                   '</div>';
+//html for resturant box
 var pageElement =   '<div id="%id%" data-toggle="modal" data-target="#modal" class="col-xs-5" style="margin:1%;">'+
                         '<img src="%imagepath%" alt="" class="img-responsive" style="max-height:300px; width:100%">'+ 
                         '<span class="hover-text"><div style="height: 75%"></div><p2>%name%</p2></span>'+
                     '</div>';
-
-                    
-
-
-
 //restaurants array
 var restaurants_array = [];
 
 $(document).ready(
     function () {
         //AJAX call to fetch random restaurants
-        
+        var restaurantUrl = "http://azizapp.eu5.org/Akkelny/fetch_restaurants.php?search=";
+        fetchRestaurants(restaurantUrl);
         //search text field
         var search_text = $('#search-text');
         
@@ -30,41 +27,55 @@ $(document).ready(
             var search_val = search_text.val();
             
             restaurants_array = [];
-            var restaurantUrl = "http://azizapp.eu5.org/Akkelny/fetch_restaurants.php?search=" + search_val;
-            displayPage(restaurantUrl, function (restaurants_array) {
-                $(pageContent).empty();
-                for (var i = 0; i < restaurants_array.length; i++)
-                {
-                    var item = makePage(pageElement, i, restaurants_array[i].name, restaurants_array[i].imagepath);
-                    $(pageContent).append(item);
-                    
-                    $('#'+i).data('modalData', {array: restaurants_array[i].food, name: restaurants_array[i].name});
-                    $('#'+i).click(function(){
-                        var data = $(this).data('modalData');
-                        $('#myModalLabel').text(data.name);
-                        displayModal(data.array);
-                    });
-                }
-            });
+            restaurantUrl = "http://azizapp.eu5.org/Akkelny/fetch_restaurants.php?search=" + search_val;
+            fetchRestaurants(restaurantUrl);
         });
 
     });
 
+//
+function fetchRestaurants(restaurantUrl){
+    displayRestaurants(restaurantUrl, function (restaurants_array) {
+        $(pageContent).empty();
+        var temp = 0;
+        for (var i = 0; i < restaurants_array.length; i++)
+        {
+            if(i%2 == 0){
+                temp = i;
+                $('#pageContent').append('<div id="row'+temp+'" class="row"></div>');
+            }
+            
+            var item = makeRestaurant(pageElement, i, restaurants_array[i].name, restaurants_array[i].imagepath);
+            $('#row'+temp).append(item);
+            
+            $('#restaurant'+i).data('modalData', {array: restaurants_array[i].food, name: restaurants_array[i].name});
+            $('#restaurant'+i).click(function(){
+                var data = $(this).data('modalData');
+                $('#myModalLabel').text(data.name);
+                console.log("ll");
+                displayModal(data.array);
+            });
+        }
+    });
+}
+
+//display modal for the restaurant
 function displayModal() {
     
-    //$(currentModal).empty();
+    $(currentModal).empty();
     var array = [];
     if(arguments.length == 1){
         array = arguments[0];
     }
     for(var i = 0; i < array.length; i++)
     {
-        var item = makeItem(modalContent, array[i].imagepath, array[i].name ,array[i].description);
+        var item = makeMeal(modalContent, array[i].imagepath, array[i].name ,array[i].description);
         $(currentModal).append(item);
     }
 }
 
-function displayPage(restaurantUrl, callback) {
+//display restaurants boxes 
+function displayRestaurants(restaurantUrl, callback) {
     //AJAX call to fetch search restaurant
     $.getJSON(restaurantUrl, function (data){
 
@@ -75,7 +86,8 @@ function displayPage(restaurantUrl, callback) {
     }); 
 }
 
-function makeItem(content, imagepath, name, description){
+//make html for the meal row
+function makeMeal(content, imagepath, name, description){
     
     var currentContent = content;
       
@@ -87,11 +99,12 @@ function makeItem(content, imagepath, name, description){
     
 }
 
-function makePage(content, id, name, imagepath) {
+//make html for the restaurant box
+function makeRestaurant(content, id, name, imagepath) {
     
     var currentContent = content;
     
-    currentContent = currentContent.replace("%id%", id);      
+    currentContent = currentContent.replace("%id%", "restaurant"+id);      
     currentContent = currentContent.replace("%imagepath%", imagepath);
     currentContent = currentContent.replace("%name%", name);
 
