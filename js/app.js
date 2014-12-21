066146e024e7954a0eb4a007e13c35cd2ae093a7
abdulaziz <abdulazizalaa@ymail.com>
2014-12-21 04:41:46 +0200

    front-end fixes :hamster:
    probably i have fixed the responsiveness problem needs some testing
    website is almost ready

--- js/app.js
+++ js/app.js
@@ -1,22 +1,17 @@
 
-var modalContent = 
-    '<div class="row">' +
-        '<div class="col-md-2"><img src="%imagepath%" width="100px"></div>' +
-        '<div class="col-md-2" style="margin-top: 10%"><p3>%name%</p3></div>' +
-        '<div class="col-md-2" style="margin-top: 10%"><p3>%description% EGP</p3></div>' +
-        '<div class="col-md-2" style="margin-top: 10%"><button type="button" class="btn-confirm"><p2>Add</p2></button></div>' +
-        '<div class="col-md-3" style="margin-top: 10%"><button type="button" class="btn-confirm"><p2>Remove</p2></button></div>' +
-       '<div class="col-md-1" style="margin-top: 10%"><div id="%counter_id%"><p3>0</p3></div></div>' +
-    '</div>'; 
+var modalContent =  '<div class="row text-center">'+
+                      '<div class="col-xs-2"><img src="%imagepath%" class="img-responsive" ></div>'+
+                      '<div class="col-xs-3" style="margin-top: 2%"><p3>%name%</p3></div>'+
+                      '<div class="col-xs-2" style="margin-top: 2%"><p3>%description% EGP</p3></div>'+
+                      '<div class="col-xs-2" style="margin-top: 2%;"><button type="button" class="btn-confirm"><p2>Add</p2></button></div>'+
+                      '<div class="col-md-3 col-sm-3 col-xs-3" style="margin-top: 2%"><button type="button" class="btn-confirm"><p2>Remove</p2></button></div>'+
+                  '</div>';
+var pageElement =   '<div id="%id%" data-toggle="modal" data-target="#modal" class="col-xs-5" style="margin:1%;">'+
+                        '<img src="%imagepath%" alt="" class="img-responsive" style="max-height:300px; width:100%">'+ 
+                        '<span class="hover-text"><div style="height: 75%"></div><p2>%name%</p2></span>'+
+                    '</div>';
 
-
-var pageElement =   
-        '<div class="col-md-2">' +
-        '<div id="%id%" data-toggle="modal" data-target="#modal" class="col-md-10" style="background color: white; margin:1%;">' +
-        '<img src="%imagepath%" alt="" class="img-responsive" style="max-height:300px; width:100%"> '+
-        '<span class="hover-text"><div style="height: 80%"></div><p2>%name%</p2></span>' +
-        '</div>'+
-        '<div class="col-md-2 col-md-offset-4"></div>';
+                    
 
 
 
@@ -24,21 +19,21 @@ var pageElement =
 var restaurants_array = [];
 
 $(document).ready(
-    function(){
+    function () {
         //AJAX call to fetch random restaurants
         
         //search text field
         var search_text = $('#search-text');
         
         //search button listener
-        $('#search-button').click(function(){
+        $('#search-button').click(function () {
             var search_val = search_text.val();
             
             restaurants_array = [];
-            var restaurantUrl = "http://azizapp.eu5.org/Akkelny/fetch_restaurants.php?search="+search_val;
-            displayPage(restaurantUrl, function(restaurants_array){
+            var restaurantUrl = "http://azizapp.eu5.org/Akkelny/fetch_restaurants.php?search=" + search_val;
+            displayPage(restaurantUrl, function (restaurants_array) {
                 $(pageContent).empty();
-                for(var i = 0; i < restaurants_array.length; i++)
+                for (var i = 0; i < restaurants_array.length; i++)
                 {
                     var item = makePage(pageElement, i, restaurants_array[i].name, restaurants_array[i].imagepath);
                     $(pageContent).append(item);
@@ -57,14 +52,13 @@ $(document).ready(
 
 function displayModal() {
     
-    $(currentModal).empty();
+    //$(currentModal).empty();
     var array = [];
     if(arguments.length == 1){
         array = arguments[0];
     }
     for(var i = 0; i < array.length; i++)
     {
-        console.log(array[0].imagepath);
         var item = makeItem(modalContent, array[i].imagepath, array[i].name ,array[i].description);
         $(currentModal).append(item);
     }
